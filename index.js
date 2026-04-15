d22ad2c6fd7b0c24c21934b3178952b69d02e113
muthuveerapandi-s-2874 <muthuveerapandi.s@zohocorp.com>
2026-04-15 12:29:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
-export default async function run(input) {
-    return `🔥 Plugin working! Input: ${input}`;
-  }
+const command = process.argv[2];
+
+switch (command) {
+  case "design-lang":
+    require("./design-lang");
+    break;
+  case "ui-thinking":
+    require("./ui-thinking");
+    break;
+  case "theme":
+    require("./zoho-theme-factory");
+    break;
+  default:
+    console.log("Available commands: design-lang, ui-thinking, theme");
+}
